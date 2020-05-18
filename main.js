3b2821d2d2b4c06bbf00579267fbee6f5b1095dc
Nick Holke <nickholke@gmail.com>
2020-05-18 12:37:55 -0700

    initial solution for test case

--- main.js
+++ main.js
@@ -5,17 +5,50 @@ const readFileAsync = promisify(fs.readFile);
 readFileAsync('./input.txt', {encoding: 'utf8'})
   .then((text) => {
     const input = text.split('\n');
-      console.log('CONTENT:', input);
-    const roomDimensions = input[0];
-    let roombaLocation = input[1];
+      // console.log('CONTENT:', input);
+    const roomX = parseInt(input[0][0]);
+    const roomY = parseInt(input[0][2]);
+    let roombaLocation = input[1].split(' ').map((str) => parseInt(str));
     const instructions = input[input.length - 1];
+    let spotsCleaned = 0;
     let dirtLocations = new Set();
 
     for (let i = 2; i < input.length - 1; i++) {
       dirtLocations.add(input[i]);
     }
 
-    console.log(roomDimensions, roombaLocation, instructions, dirtLocations);
+    // console.log(roomX, roomY, roombaLocation, instructions, dirtLocations);
+    console.log(dirtLocations)
+    if (dirtLocations.has(roombaLocation.join(' '))) {
+      dirtLocations.delete(roombaLocation.join(' '));
+      spotsCleaned++;
+    }
+
+    for (let instruction of instructions) {
+      let [roombaX, roombaY] = roombaLocation;
+      if (instruction === 'N' && roombaY < roomY - 1) {
+        roombaLocation[1]++;
+      }
+
+      if (instruction === 'S' && roombaY > 0) {
+        roombaLocation[1]--;
+      }
+
+      if (instruction === 'E' && roombaX < roomX - 1) {
+        roombaLocation[0]++;
+      }
+
+      if (instruction === 'W' && roombaX > 0) {
+        roombaLocation[0]--;
+      }
+
+      if (dirtLocations.has(roombaLocation.join(' '))) {
+        dirtLocations.delete(roombaLocation.join(' '));
+        spotsCleaned++;
+      }
+    }
+
+    console.log('location : ', roombaLocation, 'spots Cleaned: ', spotsCleaned)
   })
   .catch((err) => {
       console.log('ERROR:', err);
